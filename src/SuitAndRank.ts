f43925790dd9b789f182827890e519c35a0d62da
thenem <bnem83@gmail.com>
2016-02-18 03:51:49 +0000

    add suit and rank enums

---
+++ src/SuitAndRank.ts
@@ -0,0 +1,21 @@
+
+module TsEuchre {
+
+    export enum Suit {
+
+        Clubs,
+        Diamonds,
+        Hearts,
+        Spades
+    }
+
+    export enum Rank {
+
+        Nine,
+        Ten,
+        Jack,
+        Queen,
+        King,
+        Ace
+    }
+}
